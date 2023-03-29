eb3c85ff1b8424b549dc35b1b60e99293b4916fe
ccahill <christopher.cahill@gartner.com>
2023-03-29 10:38:52 -0700

    require turbo and devise

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,8 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
+require 'turbo-sprockets-rails3'
+require 'devise'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
