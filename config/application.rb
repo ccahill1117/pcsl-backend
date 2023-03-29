2ef43cb7345855d165f432af4618a37187e465ac
ccahill <christopher.cahill@gartner.com>
2023-03-29 10:41:37 -0700

    app rb

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-require 'turbo-sprockets-rails3'
 require 'devise'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
