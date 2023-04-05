f79af87653558ad56af56c809e9ddea8843b7b90
ccahill <christopher.cahill@gartner.com>
2023-04-05 00:10:05 -0700

    index route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  # GET / 
+  def index
+    render json: { success: 'it worked', version: `git rev-parse --short HEAD`, v: 'chris version' }
+  end
 end
