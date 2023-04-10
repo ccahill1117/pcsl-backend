f6019c2f8b076664a45d86b4b3150244ee671725
Christopher Cahill <chris@christophercahill.co>
2023-04-09 22:15:51 -0700

    push up

--- db/migrate/20230408232539_clean_up_fields.rb
+++ db/migrate/20230408232539_clean_up_fields.rb
@@ -1,4 +1,13 @@
 class CleanUpFields < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address_1, :string
+    add_column :users, :address_2, :string
+
+
+  end
+
+  def down
   end
 end
