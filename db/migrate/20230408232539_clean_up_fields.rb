ef99064189b8f76c5469ef40758c303531ba6149
Christopher Cahill <chris@christophercahill.co>
2023-04-20 15:51:56 -0700

    update users

--- db/migrate/20230408232539_clean_up_fields.rb
+++ db/migrate/20230408232539_clean_up_fields.rb
@@ -4,10 +4,32 @@ class CleanUpFields < ActiveRecord::Migration[7.0]
     add_column :users, :last_name, :string
     add_column :users, :address_1, :string
     add_column :users, :address_2, :string
-
-
+    add_column :users, :state, :string
+    add_column :users, :zip_code, :string
+    add_column :users, :work_phone, :string
+    add_column :users, :home_phone, :string
+    add_column :users, :cell_phone, :string
+    add_column :users, :gender, :string
+    add_column :users, :date_of_birth, :date
+    # 
+    add_column :users, :club, :string
+    add_column :users, :us_squash_id, :string
   end
 
   def down
+    remove_column :users, :first_name
+    remove_column :users, :last_name
+    remove_column :users, :address_1
+    remove_column :users, :address_2
+    remove_column :users, :state
+    remove_column :users, :zip_code
+    remove_column :users, :work_phone
+    remove_column :users, :home_phone
+    remove_column :users, :cell_phone
+    remove_column :users, :gender
+    remove_column :users, :date_of_birth
+    # 
+    remove_column :users, :club
+    remove_column :users, :us_squash_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_232342) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_232539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,15 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_232342) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
@@ -69,18 +62,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_232342) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "state"
+    t.string "zip_code"
+    t.string "work_phone"
+    t.string "home_phone"
+    t.string "cell_phone"
+    t.string "gender"
+    t.date "date_of_birth"
+    t.string "club"
+    t.string "us_squash_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
