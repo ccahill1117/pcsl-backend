2e982b653cd246a618bfed637c8ed865404d3f54
Christopher Cahill <chris@christophercahill.co>
2023-04-25 01:38:26 -0700

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable, :omniauthable
+         :confirmable, :lockable, :trackable, :omniauthable, :jwt_authenticatable, jwt_revocation_strategy: self
 
 
   def generate_jwt
