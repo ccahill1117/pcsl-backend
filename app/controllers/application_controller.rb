7c40992c76a28f432e1cc994a6797e8a0f9c6479
Christopher Cahill <chris@christophercahill.co>
2023-05-08 12:48:09 -0700

    root get

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   # include ActionController::MimeResponds
   # GET / 
   def index
-    render json: { success: 'it worked', version: `git rev-parse --short HEAD`, v: 'chris version' }
+    render json: { success: 'PCSL is online', version: `git rev-parse --short HEAD`, v: 'chris version' }
   end
   respond_to :json
 end
