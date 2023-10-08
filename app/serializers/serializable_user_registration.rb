8c836ebc2bf7d5fb8c63a6f85c647b25bd504d6d
Christopher Cahill <chris@christophercahill.co>
2023-10-08 06:03:46 -0400

    getting hang of this jsonapi serializer thing

--- app/serializers/serializable_user_registration.rb
+++ app/serializers/serializable_user_registration.rb
@@ -1,13 +1,13 @@
 class SerializableUserRegistration < JSONAPI::Serializable::Resource
   type 'user_registrations'
 
-  attributes :id, :seasons_id, :is_captain, :division, :rank, :has_paid, :user_id
+  attributes :id, :seasons_id, :is_captain, :division, :rank, :has_paid, :user_id, :user
 
-  # attribute :date do
-  #   @object.created_at
-  # end
+  attribute :user do
+    { created_at: @object.user.created_at, first_name: @object.user.first_name}
+  end
 
-  belongs_to :user
+  belongs_to :user_registration
 
   # has_many :comments do
   #   data do
